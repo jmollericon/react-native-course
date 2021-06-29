51abdb08068d231cb93c2c0a1176f49c30b5f099
Jorge Mollericon <jlmollericon@gmail.com>
2021-06-29 18:41:19 -0400

    cilos de vida react

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-import CalculadoraV2 from './src/components/CalculadoraV2';
+import CiclosDeVida from './CiclosDeVida';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CalculadoraV2 />
+      <CiclosDeVida />
       <StatusBar style="auto" />
     </View>
   );
